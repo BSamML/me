770b30a1e677bb5645dc9e7c0c6d0f5350e6d66b
BSamML <b.sam.mendez.l@gmail.com>
2025-01-07 20:50:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+// document.querySelectorAll('#em').forEach(item => {
+//     item.addEventListener('click', event => {
+//     // Ocultar todos los divs
+//         document.querySelectorAll('.content').forEach(div => {
+//             div.classList.remove('active');
+//         });
+//         // Mostrar el div correspondiente
+//         const targetId = event.target.getAttribute('data-target');
+//         document.getElementById(targetId).classList.add('active');
+//     });
+// });
+
+document.querySelectorAll('#em').forEach(
+    item => { item.addEventListener('click', event => { 
+        // Ocultar todos los divs 
+        document.querySelectorAll('.content').forEach(div => { 
+            div.classList.remove('active'); 
+        }); 
+
+        document.querySelectorAll('.t').forEach(div => { 
+            div.classList.remove('act'); 
+        }); 
+
+        // Mostrar el div correspondiente 
+        const targetId = event.target.getAttribute('data-target'); 
+        document.getElementById(targetId).classList.add('active'); 
+
+       event.target.classList.add('act');
+       
+    }); 
+});
+
+document.getElementById('download').addEventListener('click', () => { 
+    const link = document.createElement('a'); 
+    link.href = 'file:///C:/Users/bsmen/OneDrive/Documents/CV/sam.pdf';  
+    link.download = 'samcv.pdf'; 
+    link.click(); 
+});
+
